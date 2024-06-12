b9b9fd1b7a8f413bad1ab023fb63a4e873d84ce1
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-06-12 18:00:20 +0100

    Add Salable montisation

--- README.md
+++ README.md
@@ -1,52 +1,18 @@
-## Salable Miro
+## Salable x Miro
 
-**&nbsp;ℹ&nbsp;Note**:
+This repository is intended to be used as a starting point for building monetised Miro apps with Salable.
 
-- We recommend a Chromium-based web browser for local development with HTTP. \
-  Safari enforces HTTPS; therefore, it doesn't allow localhost through HTTP.
-- For more information, visit our [developer documentation](https://developers.miro.com).
+The starter code in this repository allows a user to purchase a license for a particular Miro board. Once the board has that license, an "Add Sticky" button allows the user to add a sticky note with "Hello World!" text to the board.
 
-### How to start locally
+This rudimentary example is intended to showcase the core parts of monetising an app with Salable.
 
-- Run `npm i` to install dependencies.
-- Run `npm start` to start developing. \
-  Your URL should be similar to this example:
- ```
- http://localhost:3000
- ```
-- Paste the URL under **App URL** in your
-  [app settings](https://developers.miro.com/docs/build-your-first-hello-world-app#step-3-configure-your-app-in-miro).
-- Open a board; you should see your app in the app toolbar or in the **Apps**
-  panel.
+If you want to learn more about Salable, read the [Quick Start Guide](https://docs.salable.app/docs/quick-start-guide) and [The Salable Way](https://docs.salable.app/docs/the-salable-way).
 
-### How to build the app
+## Setup
 
-- Run `npm run build`. \
-  This generates a static output inside [`dist/`](./dist), which you can host on a static hosting
-  service.
+1. Clone this repository.
+2. `cp .env.example .env` and fill in the variables with values from your
+   Salable product.
+3. `npm start`
 
-### Folder structure
-
-<!-- The following tree structure is just an example -->
-
-```
-.
-├── src
-│  ├── assets
-│  │  └── style.css
-│  ├── app.tsx      // The code for the app lives here
-│  └── index.ts    // The code for the app entry point lives here
-├── app.html       // The app itself. It's loaded on the board inside the 'appContainer'
-└── index.html     // The app entry point. This is what you specify in the 'App URL' box in the Miro app settings
-```
-
-### About the app
-
-This sample app provides you with boilerplate setup and configuration that you can further customize to build your own app.
-
-<!-- describe shortly the purpose of the sample app -->
-
-Built using [`create-miro-app`](https://www.npmjs.com/package/create-miro-app).
-
-This app uses [Vite](https://vitejs.dev/). \
-If you want to modify the `vite.config.js` configuration, see the [Vite documentation](https://vitejs.dev/guide/).
+If you need any more detailed instructions on setting up a new Miro app, read the [Miro: Build Your First Hello World App](https://developers.miro.com/docs/build-your-first-hello-world-app) guide.
--- src/app.tsx
+++ src/app.tsx
@@ -1,47 +1,99 @@
-import * as React from 'react';
-import {createRoot} from 'react-dom/client';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { getCheckoutLink, getGrantee } from "@salable/js";
 
-import '../src/assets/style.css';
+import "../src/assets/style.css";
+import { BoardInfo } from "@mirohq/websdk-types";
 
 async function addSticky() {
   const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
+    content: "Hello, World!",
   });
 
   await miro.board.viewport.zoomTo(stickyNote);
 }
 
 const App: React.FC = () => {
+  const [checkoutLink, setCheckoutLink] = React.useState<string | null>(null);
+  const [canAddSticky, setCanAddSticky] = React.useState(false);
+
+  const apiKey = import.meta.env.VITE_SALABLE_API_KEY as string;
+
+  const checkUserLicense = async (board: BoardInfo) => {
+    const { hasCapability } = await getGrantee({
+      apiKey,
+      productUuid: import.meta.env.VITE_SALABLE_PRODUCT_UUID as string,
+      granteeId: board.id,
+    });
+
+    setCanAddSticky(hasCapability("Create"));
+    return hasCapability("pro");
+  };
+
+  const fetchCheckoutLink = async (boardInfo: BoardInfo) => {
+    if (checkoutLink) return;
+
+    const boardUrl = `https://miro.com/app/board/${boardInfo.id}/`;
+
+    const url = await getCheckoutLink({
+      apiKey,
+      planUuid: import.meta.env.VITE_SALABLE_PLAN_UUID as string,
+      // The grantee can be anything, in this case we've opted to have the
+      // license belong to the board.
+      granteeId: boardInfo.id,
+      member: boardInfo.id,
+      successUrl: boardUrl,
+      cancelUrl: boardUrl,
+    });
+
+    setCheckoutLink(url);
+    await checkUserLicense(boardInfo);
+  };
+
+  async function setup() {
+    const boardInfo = await miro.board.getInfo();
+    const isProMember = await checkUserLicense(boardInfo);
+    if (!isProMember) {
+      await fetchCheckoutLink(boardInfo);
+    }
+  }
+
   React.useEffect(() => {
-    addSticky();
+    setup();
   }, []);
 
   return (
-    <div className="grid wrapper">
-      <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
-      </div>
-      <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
-      </div>
-      <div className="cs1 ce12">
-        <a
+    <div>
+      {checkoutLink && !canAddSticky ? (
+        <>
+          <p>In order to use this app, you need to have a Pro license.</p>
+          <a
+            href={checkoutLink}
+            target="_blank"
+            className="button button-primary"
+          >
+            Purchase
+          </a>
+          <hr />
+        </>
+      ) : null}
+
+      {canAddSticky ? <p>You are an active Pro license holder.</p> : null}
+
+      <div>
+        <button
+          onClick={addSticky}
           className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
+          disabled={!canAddSticky}
         >
-          Read the documentation
-        </a>
+          {!canAddSticky ? <span className="icon icon-deactivated" /> : null}
+          Add sticky!
+        </button>
       </div>
     </div>
   );
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 root.render(<App />);
